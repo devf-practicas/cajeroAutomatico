155ef551f58e44cd024693956b39bf0fae66ffcb
Ever Dávila <github@davila.uno>
2021-08-26 22:38:14 -0500

    demo robot2

--- js/cajero.js
+++ js/cajero.js
@@ -3,9 +3,7 @@
     {name: "Carlos", saldo:900, password: "hello"}, {name: "Cecilia", saldo: 970, password: "12345"}, 
     {name: "Ever", saldo: 899, password: "classroom"}, {name: "Stephano", saldo: 900, password: "9876"}
 ]
- 
-
-*/
+ */
 
 function validarDatos(){
     var user = document.getElementById("user").value
