98543981d1df947a5854048fa64c26f71a360372
caritama <carlosaranda.50@gmail.com>
2021-09-02 18:01:32 -0500

    cambios al código anterior

--- js/cajero.js
+++ js/cajero.js
@@ -1,150 +1,168 @@
+//funcion para que se recargue despues de haber construido el div de cuentas
+jQuery(document).ready(function($) {
+	divCuentas();
 
-/*const Accounts = [
-    {name: "Carlos", saldo:900, password: "hello"}, {name: "Cecilia", saldo: 970, password: "12345"}, 
-    {name: "Ever", saldo: 899, password: "classroom"}, {name: "Stephano", saldo: 900, password: "9876"}
-]
- */
-
-function validarDatos(){
-    var user = document.getElementById("user").value
-    var pass = document.getElementById("password").value
-    
-
-    if(user=="carlos" && pass=="carlos"){
-        window.location="/dashboard/"; 
-        //alert("Hola Carlos ")
+    toastMsg('Bienvenido a su Banca Digital', 'Aquí podrá hacer todas sus operaciones bancarias');
+});
+//Array de cuentas
+var Accounts = [
+    {
+    	cuenta: '100100320503052',
+    	name:"Carlos Ritter", 
+    	saldo:500.20, 
+    	password:"carlos", 
+    	user:"carlos"
+    }, {
+    	cuenta: '100100320503053',
+    	name: "Cecilia",
+    	saldo: 570.00, 
+    	password: "cecilia",
+    	user:"cecilia"
+    }, {
+    	cuenta: '100100320503054',
+    	name:"Ever", 
+    	saldo: 499.30, 
+    	password: "ever", 
+    	user:"ever"
+    }, {
+    	cuenta: '100100320503055',
+    	name: "Stephano", 
+    	saldo: 302.20,
+    	password: "stephano", 
+    	user:"stephano"
     }
+];
+//valores a tener en cuenta
+let max = 990;
+let min = 10;
 
-    else if(user=="cecilia" && pass=="cecilia"){
-        window.location="/dashboard/"; 
-        //alert("Hola Cecilia")
-    }
+const toastMsg = (titulo, msg) =>{
+    const mensaje = document.querySelector("#mensaje");
+    let toastHtml ='<div class="toast-container position-fixed top-0 end-0 p-3" style="z-index: 99999">\
+        <div id="basicToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">\
+            <div class="toast-header bg-primary text-light">\
+            <h5 class="my-0">'+titulo+'</h5>\
+            </div>\
+            <div class="toast-body">'+msg+'</div>\
+        </div>  \
+    </div>';
 
-    else if(user=="stephano" && pass=="stephano"){
-        window.location="/dashboard/"; 
-        //alert("Hola Estephano")
-    }
+    mensaje.innerHTML = toastHtml;
 
-
-    else if(user=="ever" && pass=="ever"){
-        window.location="/dashboard/newindex.html";  
-    }
-
-    else if(user==="" && pass===""){
-        alert("Por favor ingresa un usuario y un password")
-    }
-
-
-    else{
-        alert("usuario o  password incorrectos")
-    }
-    return false;  // Add return false, because submitting a form will refresh the page return false will keep the page static and your window.location.href will redirect.
-} 
-function consultarSaldo(){
-    const Cantidad=document.getElementById('cuenta').value
-    alert("Su saldo en cuenta es: "+Cantidad)
-}
-function ingresarMonto(){
-    const Cantidad = parseFloat(document.getElementById('monto1').value)
-    const Cantidad1 = parseFloat(document.getElementById('cuenta').value)
-    document.getElementById('cuenta').value = parseFloat(Cantidad + Cantidad1)
-    const saldo = parseFloat(Cantidad + Cantidad1)
-    if(saldo>991){
-        const ingreso = 990 - Cantidad1
-        alert("Su ingreso excede nuestra politica, puede ingresar hasta: "+ ingreso)
-        document.getElementById('cuenta').value = parseFloat(Cantidad1)
-    }else{
-        alert("Su monto ingresado es: "+Cantidad)
-        alert("Su saldo es: "+saldo)
-    }
-    
-}
-function retirarMonto(){
-    const Cantidad = parseFloat(document.getElementById('monto1').value)
-    const Cantidad1 = parseFloat(document.getElementById('cuenta').value)
-    document.getElementById('cuenta').value = parseFloat( Cantidad1 - Cantidad)
-    const saldo = parseFloat(Cantidad1 - Cantidad)
-    if(saldo<10){
-        const egreso = Cantidad1 - 10
-        alert("Su egreso excede nuestra politica, puede retirar hasta: "+ egreso)
-        document.getElementById('cuenta').value = parseFloat(Cantidad1)
-    }else{
-        alert("Su monto a retirar es: "+Cantidad)
-        alert("Su saldo es: "+saldo)
-    }
-}
-function cerrarSesion () {
-    const Sesion = document.getElementById('cerrar_id')
-    window.location = "/"
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
 }
 
-//Interface Ever
+//creamos los div de cada cuenta del array
+const divCuentas = () =>{
+	const homeCuentas = document.querySelector("#homeCuentas");
+	let html = '';
+	for (var i = 0; i < Accounts.length; i++) {
+		persona = Accounts[i];
 
-function consultarSaldos(){
-    const Cantidades=document.getElementById('consultas_id').value
-    toastr.info('Estimado Dogecliente el saldo en su cuenta es de $'+Cantidades +' Dogecoins')
-    //alert("Su saldo en cuenta es: "+Cantidades)//
+		html +='<div class="col">\
+	        <div class="card mb-4 rounded-3 shadow-sm">\
+	          <div class="card-header py-3">\
+	            <h4 class="my-0 fw-normal">'+ persona.name +'</h4>\
+	          </div>\
+	          <div class="card-body">\
+	            <ul class="list-unstyled mt-3 mb-4">\
+	              <li>Nro Cuenta <br> ' + persona.cuenta + '</li>\
+	            </ul>\
+	            <button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="elegirCuenta('+ persona.cuenta +')">Log In</button>\
+	          </div>\
+	        </div>\
+	      </div>';
+
+	}
+
+	homeCuentas.innerHTML = html;
+	
 }
-function ingresarMontos(){
-    const Cantidades = parseFloat(document.getElementById('insertar_dogecoin').value)
-    const Cantidades1 = parseFloat(document.getElementById('consultas_id').value)
-    document.getElementById('consultas_id').value = parseFloat(Cantidades + Cantidades1)
-    const saldos = parseFloat(Cantidades + Cantidades1)
-    if(saldos>991){
-        const ingresos = 990 - Cantidades1
-        toastr.error('Tu ingreso excede nuestra Dogepolitica, puedes ingresar hasta: $'+ ingresos+' Dogecoins')
-        document.getElementById('consultas_id').value = parseFloat(Cantidades1)
-    }
-    
-    else if(document.getElementById('insertar_dogecoin').value===""){
-        toastr.warning('Estimado Dogecliente, ingresa el monto que deseas depositar', {
-            "progressBar": true,
-            "closeButton": true,
-        })
-        document.getElementById('consultas_id').value = parseFloat(Cantidades1)
-    }
-    else if(document.getElementById('insertar_dogecoin').value<0){
-        toastr.error('No puedes depositar cantidades negativas, ingresa un valor positivo','Tenemos un código 3312', {
-            "progressBar": true,
-            "closeButton": true,
-        }) 
-        document.getElementById('consultas_id').value = parseFloat(Cantidades1)
-    }
-    else{
-        toastr.info('Tu nuevo saldo es $'+saldos +' Dogecoins')
-        toastr.success('Depositaste de manera exitosa $'+Cantidades +' Dogecoins')
-        //alert("Depositaste: "+Cantidades +" Dogecoins")//
-        //alert("Tu  nuevo saldo es: "+saldos +" Dogecoins")//
-    }
-    
+//fin de los div
+//funcion para elegir la cuenta
+const elegirCuenta = (cuenta) => {
+	document.getElementById("cuenta").value = cuenta;
+	login(cuenta);
 }
-function retirarMontos(){
-    const Cantidades = parseFloat(document.getElementById('retirar_dogecoin').value)
-    const Cantidades1 = parseFloat(document.getElementById('consultas_id').value)
-    document.getElementById('consultas_id').value = parseFloat( Cantidades1 - Cantidades)
-    const saldos = parseFloat(Cantidades1 - Cantidades)
-    if(saldos<10){
-        const egresos = Cantidades1 - 10
-        toastr.warning('Tu retiro excede nuestra Dogepolitica, puedes retirar hasta $'+ egresos+'Dogecoins')
-        //alert("Tu egreso excede nuestra Dogelitica, puedes retirar hasta: "+ egresos +" Dogecoins")//
-        document.getElementById('consultas_id').value = parseFloat(Cantidades1)
-    }
-    else if(document.getElementById('retirar_dogecoin').value===""){
-    toastr.warning('Estimado Dogecliente, ingresa el monto que deseas retirar')
-        //alert("Ingresa el monto que deseas retirar")//
-        document.getElementById('consultas_id').value = parseFloat(Cantidades1)
-    }
-    else if(document.getElementById('retirar_dogecoin').value<0){
-        toastr.error('No puedes retirar cantidades negativas, ingresa un valor positivo','Tenemos un código 3312', {
-            "progressBar": true,
-            "closeButton": true,
-        })
-        document.getElementById('consultas_id').value = parseFloat(Cantidades1)
-    } 
-    else{
-        toastr.info('Tu nuevo saldo es $'+saldos +' Dogecoins')
-        toastr.success('Retiraste de manera exitosa$'+Cantidades +' Dogecoins')
-        //alert("Estas retiranto: "+Cantidades +" Dogecoins")//
-        //alert("Tu  nuevo saldo es: "+saldos +" Dogecoins")//
-    }
+//funcion para desplegar el modal del login
+const login = (cuenta) => {
+	$('#staticBackdrop').modal('show');
+}
+//funcion para validar los datos ingresados en el login
+const validarDatos = () => {
+	user = document.getElementById("user").value;
+	pass = document.getElementById("pass").value;
+	cta = document.getElementById("cuenta").value;
+	
+	let cont = 0;
+	for (var i = 0; i < Accounts.length; i++) {
+		persona = Accounts[i];
+
+		if(user == persona.user && pass == persona.password && cta == persona.cuenta){
+			cont++;
+			document.getElementById("saldoOculto").value = persona.saldo;
+			document.getElementById("persona").innerHTML = 'Hola '+persona.name
+		}
+	}
+
+	if(cont > 0){
+		//ocultamos el contenido de elegir cuenta
+		$('#home').css('display', 'none');
+
+		//visualizamos el contenido movimientos
+		$('#movimientos').removeAttr('style');
+
+		//Ocultamos el login
+		$('#staticBackdrop').modal('hide');
+	}else{
+		toastMsg('Datos incorrectos','Hola, el usuario o la contraseña son incorrectos');
+	}
+}
+//funcion para las validaciones dentro del detalle de las cuentas
+const movimientos = (accion) => {
+
+	saldo = document.getElementById("saldoOculto").value;
+	saldo = parseFloat(saldo);
+
+	if(accion == 'consultar'){
+		document.getElementById("saldo").value = saldo;
+		alert('Su saldo es: '+saldo)
+	}else if(accion == 'depositar'){
+		 
+		//Obtenemos el monto a depositar
+		montoDepositar = document.getElementById("deposito").value;
+		montoDepositar = parseFloat(montoDepositar);
+
+		saldoTotal = saldo + montoDepositar;
+
+		puedeDepositar = max - saldo;
+
+		if(saldoTotal > max){
+			alert('Excede el monto a depositar, solo puede depositar $ '+puedeDepositar);
+			
+		} else if (montoDepositar < 0 || montoDepositar == '') {
+			alert('Debe ingresar un monto valido')
+		}else{
+			document.getElementById("saldoOculto").value = saldoTotal;
+			document.getElementById("deposito").value = 0;
+			alert('Monto depositado: $' + montoDepositar + '. Saldo disponible: $ '+ saldoTotal);
+		}
+
+	}else{
+		montoRetirar = document.getElementById("retiro").value;
+		montoRetirar = parseFloat(montoRetirar);
+
+		saldoTotal = saldo - montoRetirar;
+
+		puedeRetirar = saldo - min;
+		if(saldoTotal < min){
+			alert('Excede el monto a retirar, solo puede retirar $ '+puedeRetirar);
+		}else if (montoRetirar < 0 || montoRetirar == '') {
+			alert('Debe ingresar un monto valido')
+		}else{
+			document.getElementById("saldoOculto").value = saldoTotal;
+			document.getElementById("retiro").value = 0;
+			alert('Monto retirado: $' + montoRetirar + '. Saldo disponible: $ '+ saldoTotal);
+		}
+	}
 }
